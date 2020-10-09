cabc9e95eef09eccaf45f422449ddb0e48b92b30
Rajeev Ravindran <razorrajeev@gmail.com>
2020-10-10 01:17:16 +0530

    Initial commit

---
+++ cptc-install.sh
@@ -0,0 +1,13 @@
+## Direct binary installs
+apt update
+apt install ftp hydra-gtk gobuster dnsrecon sublist3r nikto cadaver enum4linux smbmap smbclient onesixtyone snmpenum sqsh default-mysql-client postgresql-client -y --upgrade
+
+## Python Virtual Environment
+apt install python3.7
+pip3 install virtualenv
+virtualenv cptc-tools
+source cptc-tools/bin/activate
+
+## Python Specific tools
+
+
