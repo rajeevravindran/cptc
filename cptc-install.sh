0dc4dca3f02541171c917882876bdd3ac7d52288
Rajeev Ravindran <razorrajeev@gmail.com>
2020-10-10 01:27:56 +0530

    adding pip

--- cptc-install.sh
+++ cptc-install.sh
@@ -3,7 +3,7 @@ apt update
 apt install ftp hydra-gtk gobuster dnsrecon sublist3r nikto cadaver enum4linux smbmap smbclient onesixtyone snmpenum sqsh default-mysql-client postgresql-client -y --upgrade
 
 ## Python Virtual Environment
-apt install python3.7
+apt install python3.7 python3-pip
 pip3 install virtualenv
 virtualenv cptc-tools
 source cptc-tools/bin/activate
