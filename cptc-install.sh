ffb8a96737803888216b0ac90148b30dc06a0d91
Rajeev Ravindran <razorrajeev@gmail.com>
2020-10-16 19:33:39 +0530

    dnsrecon namp install via apt, takes long in ptf

--- cptc-install.sh
+++ cptc-install.sh
@@ -1,6 +1,6 @@
 ## Direct binary installs
 apt update
-apt install ftp cadaver snmpenum sqsh default-mysql-client postgresql-client -y --upgrade
+apt install ftp cadaver snmpenum sqsh default-mysql-client postgresql-client nmap dnsrecon -y --upgrade
 
 ## Python Environment
 apt install python3.7 python3-pip
@@ -9,11 +9,8 @@ apt install python3.7 python3-pip
 
 git clone https://github.com/trustedsec/ptf.git
 echo "modules/exploitation/metasploit
-modules/vulnerability-analysis/nmap
-modules/exploitation/metasploit.py
 modules/vulnerability-analysis/hydra
 modules/intelligence-gathering/sublist3r
-modules/intelligence-gathering/dnsrecon
 modules/vulnerability-analysis/nikto
 modules/intelligence-gathering/gobuster
 modules/intelligence-gathering/enum4linux
